fb709d7448b10795a25323a6b03334da35283ab2
jacobshaw42 <jacob.shaw42@gmail.com>
2024-03-21 23:24:24 -0500

    adding conditions for create and delete and another policies file for redshift

--- create_resources.sh
+++ create_resources.sh
@@ -2,19 +2,30 @@
 source config.sh
 
 # create the s3 bucket and download source data files
-aws s3api create-bucket --bucket $s3name --region $region --create-bucket-configuration '{"LocationConstraint":"us-west-2"}'
-aws s3 cp s3://openalex/data/institutions/ s3://$s3name/institutions/ --recursive
-aws s3 cp s3://openalex/data/publishers/ s3://$s3name/publishers/ --recursive
+check_s3=$(aws s3api list-buckets --output text --query 'Buckets[?Name==`open-alex-js0258`].Name')
+if [[ $check_s3 != $s3name ]] 
+then
+    aws s3api create-bucket --bucket $s3name --region $region --create-bucket-configuration '{"LocationConstraint":"us-west-2"}'
+    aws s3 cp s3://openalex/data/institutions/ s3://$s3name/institutions/ --recursive
+    aws s3 cp s3://openalex/data/publishers/ s3://$s3name/publishers/ --recursive
+else
+    echo "S3 Bucket $s3name already exists"
+fi
 
 # create and assigne iam roles and policies
 echo "creating $redshift_role_name"
-aws iam create-role --role-name $redshift_role_name --region $region --assume-role-policy-document file://iam_policies/redshift_trust_policy.json
-POLICY_ARN=$(aws iam create-policy --policy-name $redshift_role_name --policy-document file://iam_policies/redshift_access.json --output text --query Policy.Arn)
-aws iam attach-role-policy --role-name $redshift_role_name --policy-arn $POLICY_ARN
+check_role=$(aws iam list-roles --output text --query 'Roles[?RoleName==`AmazonRedshift-CommandsAccessRole`].RoleName')
+if [[ $check_role != $redshift_role_name]]
+then
+    aws iam create-role --role-name $redshift_role_name --region $region --assume-role-policy-document file://iam_policies/redshift_trust_policy.json
+    POLICY_ARN=$(aws iam create-policy --policy-name $redshift_role_name --policy-document file://iam_policies/redshift_access.json --output text --query Policy.Arn)
+    aws iam attach-role-policy --role-name $redshift_role_name --policy-arn $POLICY_ARN
+else
+    echo "Roles already created"
 
 # create and assign security groups
 
 
 # create redshift
-#aws redshift create-cluster --cluster-identifier open-alex-js-rs --db-name open-alex-js --node-type dc2.large \
-#     --number-of-nodes 1 --master-username redshift1 --master-user-password $redshift_password --iam-roles $POLICY_ARN \
+aws redshift create-cluster --cluster-identifier open-alex-js-rs --db-name open-alex-js --node-type dc2.large \
+     --number-of-nodes 1 --master-username redshift1 --master-user-password $redshift_password --default-iam-role-arn $POLICY_ARN \
--- delete_resources.sh
+++ delete_resources.sh
@@ -2,14 +2,32 @@
 source config.sh
 
 # delete s3
-aws s3 rm s3://open-alex-js0258/ --recursive
-aws s3api delete-bucket --bucket open-alex-js0258
+check_s3=$(aws s3api list-buckets --output text --query 'Buckets[?Name==`open-alex-js0258`].Name')
+if [[ $check_s3 == $s3name ]] 
+then
+    aws s3 rm s3://open-alex-js0258/ --recursive
+    aws s3api delete-bucket --bucket open-alex-js0258
+else
+    echo "S3 Bucket $s3name does not exist"
+fi
 
 # delete roles and policies
-POLICY_ARN=$(aws iam list-policies --output text --query 'Policies[?PolicyName==`AmazonRedshift-CommandsAccessRole`].Arn')
-aws iam detach-role-policy --role-name AmazonRedshift-CommandsAccessRole --policy-arn $POLICY_ARN
-aws iam delete-policy --policy-arn $POLICY_ARN
-aws iam delete-role --role-name AmazonRedshift-CommandsAccessRole
+check_role=$(aws iam list-roles --output text --query 'Roles[?RoleName==`AmazonRedshift-CommandsAccessRole`].RoleName')
+if [[ $check_role == $redshift_role_name ]]
+then
+    POLICY_ARN=$(aws iam list-policies --output text --query 'Policies[?PolicyName==`AmazonRedshift-CommandsAccessRole`].Arn')
+    aws iam detach-role-policy --role-name AmazonRedshift-CommandsAccessRole --policy-arn $POLICY_ARN
+    aws iam delete-policy --policy-arn $POLICY_ARN
+    aws iam delete-role --role-name AmazonRedshift-CommandsAccessRole
+else
+    echo "Role does not exist"
+fi
 
 # delete redshift
-aws redshift delete-cluster --cluster-identifier open-alex-js-rs
+redshift_check=$(aws redshift describe-clusters --output text --query 'Clusters')
+if [[ $redshift_check != "" ]]
+then
+    aws redshift delete-cluster --cluster-identifier open-alex-js-rs
+else
+    echo "No redshift clusters"
+fi
