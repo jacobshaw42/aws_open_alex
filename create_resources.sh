a2910a17f2fb7b9d94fe5214369fb02a615e7eb3
jacobshaw42 <jacob.shaw42@gmail.com>
2024-03-31 21:39:34 -0500

    adding local pyspark setup and decompressing downloads

--- create_resources.sh
+++ create_resources.sh
@@ -6,8 +6,15 @@ check_s3=$(aws s3api list-buckets --output text --query 'Buckets[?Name==`open-al
 if [[ $check_s3 != $s3name ]] 
 then
     aws s3api create-bucket --bucket $s3name --region $region --create-bucket-configuration '{"LocationConstraint":"us-west-2"}'
-    aws s3 cp s3://openalex/data/institutions/ s3://$s3name/institutions/ --recursive
-    aws s3 cp s3://openalex/data/publishers/ s3://$s3name/publishers/ --recursive
+    #aws s3 cp s3://openalex/data/institutions/ s3://$s3name/institutions/ --recursive
+    aws s3 cp s3://openalex/data/institutions/ tmp_data/institutions/ --recursive
+    aws s3 cp s3://openalex/data/publishers/ tmp_data/publishers/ --recursive
+    gunzip tmp_data/institutions/*/*.gz
+    gunzip tmp_data/publishers/*/*.gz
+    aws s3 cp tmp_data/institutions/  s3://$s3name/institutions/ --recursive --exclude "*" --include "*part*"
+    aws s3 cp tmp_data/publishers/ s3://$s3name/publishers/ --recursive --exclude "*" --include "*part*"
+    rm -r tmp_data/institutions/updated_date\=202*
+    rm -r tmp_data/publishers/updated_date\=202*
 else
     echo "S3 Bucket $s3name already exists"
 fi
@@ -15,17 +22,22 @@ fi
 # create and assigne iam roles and policies
 echo "creating $redshift_role_name"
 check_role=$(aws iam list-roles --output text --query 'Roles[?RoleName==`AmazonRedshift-CommandsAccessRole`].RoleName')
-if [[ $check_role != $redshift_role_name]]
+if [[ $check_role != $redshift_role_name ]]
 then
-    aws iam create-role --role-name $redshift_role_name --region $region --assume-role-policy-document file://iam_policies/redshift_trust_policy.json
+    echo "creating redshift role"
+    ROLE_NAME=$(aws iam create-role --role-name $redshift_role_name --region $region --assume-role-policy-document file://iam_policies/redshift_trust_policy.json --output text --query Role.RoleName)
+    echo "creating redshift policy"
     POLICY_ARN=$(aws iam create-policy --policy-name $redshift_role_name --policy-document file://iam_policies/redshift_access.json --output text --query Policy.Arn)
+    echo "attaching redshift policy to role"
     aws iam attach-role-policy --role-name $redshift_role_name --policy-arn $POLICY_ARN
 else
     echo "Roles already created"
+fi
 
 # create and assign security groups
 
+echo "now let's create the redshift instance"
 
 # create redshift
-aws redshift create-cluster --cluster-identifier open-alex-js-rs --db-name open-alex-js --node-type dc2.large \
-     --number-of-nodes 1 --master-username redshift1 --master-user-password $redshift_password --default-iam-role-arn $POLICY_ARN \
+# aws redshift create-cluster --cluster-identifier open-alex-js-rs --db-name open-alex-js --node-type dc2.large \
+#      --number-of-nodes 1 --master-username redshift1 --master-user-password $redshift_password --default-iam-role-arn $POLICY_ARN \
---
+++ local_pyspark/install_package.sh
@@ -0,0 +1,5 @@
+pip install -r requirement.txt
+
+# to start pyspark with S3 compability
+# pyspark --packages com.amazonaws:aws-java-sdk-pom:1.10.34,org.apache.hadoop:hadoop-aws:3.4.0
+# pyspark --packages com.amazonaws:aws-java-sdk-bundle:1.12.603,org.apache.hadoop:hadoop-aws:3.4.0,org.apache.hadoop:hadoop-common:3.4.0
